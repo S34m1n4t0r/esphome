9a4ed79064e664de83afcc025e3a2fc5c055fbca
S34m1n4t0r <81908108+S34m1n4t0r@users.noreply.github.com>
2024-01-26 23:06:26 +0100

    Update waveshare_epaper.h - whitspaces

--- esphome/components/waveshare_epaper/waveshare_epaper.h
+++ esphome/components/waveshare_epaper/waveshare_epaper.h
@@ -582,7 +582,6 @@ class WaveshareEPaper4P2InV2 : public WaveshareEPaper {
   int get_height_internal() override;
 };
 
-
 class WaveshareEPaper4P2InBV2 : public WaveshareEPaper {
  public:
   void initialize() override;
